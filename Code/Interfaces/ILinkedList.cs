27d62396b12c680f4595dbce873f60b671d6ee1b
Wizacass <visak.pet0@gmail.com>
2020-02-29 21:25:02 +0200

    Partially implement Doubly Linked List

--- Code/Interfaces/ILinkedList.cs
+++ Code/Interfaces/ILinkedList.cs
@@ -1,8 +1,18 @@
 public interface ILinkedList<T>
 {
+    T First();
+
+    T Last();
+
+    void AddFirst(T element);
+
+    void AddLast(T element);
+
+    void Remove(T element);
+
+    bool Contains(T element);
+
     T[] ToArray();
 
     void sort();
-
-    ILinkedList<T> sorted();
 }
--- Code/MyLinkedList.cs
+++ Code/MyLinkedList.cs
@@ -4,28 +4,132 @@ using System.Collections.Generic;
 
 public class MyLinkedList<T> : ILinkedList<T>, IEnumerable<T>
 {
-    public IEnumerator<T> GetEnumerator()
+    public int Size { get; private set; }
+    public bool IsEmpty => Size == 0;
+
+    private Node _head;
+    private Node _tail;
+
+    public MyLinkedList()
     {
-        throw new NotImplementedException();
+        Size = 0;
+    }
+
+    public void AddFirst(T element)
+    {
+        var node = new Node(element);
+        if (IsEmpty)
+        {
+            _head = node;
+            _tail = node;
+        }
+        else
+        {
+            node.Next = _head;
+            _head.Previous = node;
+            _head = node;
+        }
+        Size++;
+    }
+
+    public void AddLast(T element)
+    {
+        var node = new Node(element);
+        if (IsEmpty)
+        {
+            _head = node;
+            _tail = node;
+        }
+        else
+        {
+            node.Previous = _tail;
+            _tail.Next = node;
+            _tail = node;
+        }
+        Size++;
+    }
+
+    public bool Contains(T element)
+    {
+        if (IsEmpty) { return false; }
+        foreach (var obj in this)
+        {
+            if (element.Equals(obj))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public T First()
+    {
+        return _head.Data;
+    }
+
+    public T Last()
+    {
+        return _tail.Data;
+    }
+
+    public void Remove(T element)
+    {
+        if (IsEmpty) { return; }
+        for (var node = _head; node != null; node = node.Next)
+        {
+            if (element.Equals(node.Data))
+            {
+                node.Previous.Next = node.Next;
+                node.Next.Previous = node.Previous;
+                node = null;
+                Size--;
+                return;
+            }
+        }
     }
 
     public void sort()
     {
-        throw new NotImplementedException();
-    }
-
-    public ILinkedList<T> sorted()
-    {
+        if (IsEmpty) { return; }
         throw new NotImplementedException();
     }
 
     public T[] ToArray()
     {
-        throw new NotImplementedException();
+        if (IsEmpty) { return null; }
+        var array = new T[Size];
+        int i = 0;
+        foreach (var obj in this)
+        {
+            array[i++] = obj;
+        }
+        return array;
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        for (var node = _head; node != null; node = node.Next)
+        {
+            yield return node.Data;
+        }
     }
 
     IEnumerator IEnumerable.GetEnumerator()
     {
-        throw new NotImplementedException();
+        return GetEnumerator();
+    }
+
+    private sealed class Node
+    {
+        public T Data { get; }
+        public Node Previous { get; set; }
+        public Node Next { get; set; }
+
+        public Node(T data)
+        {
+            Data = data;
+            Previous = null;
+            Next = null;
+        }
     }
 }
